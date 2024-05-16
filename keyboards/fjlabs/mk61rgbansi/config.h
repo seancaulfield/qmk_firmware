f6d8ba168056fed36426e2b64f3aa00330b7abf4
Sean Caulfield <sean@yak.net>
2024-05-15 21:02:44 -0400

    change up layout and rgb effects for mk61

--- keyboards/fjlabs/mk61rgbansi/config.h
+++ keyboards/fjlabs/mk61rgbansi/config.h
@@ -44,8 +44,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 // For full list of effects, see:
 // https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
 // #define ENABLE_RGB_MATRIX_ALPHAS_MODS
-// #define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-// #define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
+#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
 //#define ENABLE_RGB_MATRIX_BREATHING
 // #define ENABLE_RGB_MATRIX_BAND_SAT
 // #define ENABLE_RGB_MATRIX_BAND_VAL
