7cf1314013485a9838c0447d235787ddc1a07c41
Sean Caulfield <sean@yak.net>
2025-03-29 10:38:59 -0400

    add personal keymap and attempt at making RGB work

--- keyboards/tab/swagkeys/transition_lite_1k/config.h
+++ keyboards/tab/swagkeys/transition_lite_1k/config.h
@@ -22,11 +22,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define WS2812_PWM_COMPLEMENTARY_OUTPUT                 /** TIMx_CHyN   需要带N结尾*/
 #define WS2812_PWM_DRIVER           PWMD1               /** TIMx        对应x*/
 #define WS2812_PWM_CHANNEL          1                   /** TIMx_CHy    对应y */
-#define WS2812_PWM_PAL_MODE         2
+//#define WS2812_PWM_PAL_MODE         2
+#define WS2812_PWM_PAL_MODE         1
 #define WS2812_DMA_STREAM           STM32_DMA1_STREAM5  /** TIMx_UP     对应x的UP，DMA数据表*/
 #define WS2812_DMA_CHANNEL          5                   /** STM32_DMAx_STREAMy 对应y*/
 
-#define RGB_MATRIX_DISABLE_KEYCODES
+//#define RGB_MATRIX_DISABLE_KEYCODES
 
 #define RGB_INDEX_CAPS                  62  /** Caps 指示灯索引 **/
 #define RGB_INDEX_SCROLL                14  /** Scroll 指示灯索引 **/
