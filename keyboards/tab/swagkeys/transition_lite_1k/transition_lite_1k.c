43b718c1db55076ac66af36417f598fb66c799f4
Sean Caulfield <sean@yak.net>
2025-03-29 21:27:13 -0400

    working bitbang underglow

--- keyboards/tab/swagkeys/transition_lite_1k/transition_lite_1k.c
+++ keyboards/tab/swagkeys/transition_lite_1k/transition_lite_1k.c
@@ -69,10 +69,38 @@ bool rgb_matrix_indicators_kb(void) {
  *
  */
 
+#ifdef RGBLIGHT_ENABLE
+
+#define WS2812_BITBANG_NOP_FUDGE (0.4)
+#define WS2812_RGBLIGHT_DI_PIN (A2)
+
+#ifndef WS2812_RES
+#    define WS2812_RES (1000 * WS2812_TRST_US) // Width of the low gap between bits to cause a frame to latch
+#endif
+
+#define NUMBER_NOPS 6
+#define CYCLES_PER_SEC (CPU_CLOCK / NUMBER_NOPS * WS2812_BITBANG_NOP_FUDGE)
+#define NS_PER_SEC (1000000000L) // Note that this has to be SIGNED since we want to be able to check for negative values of derivatives
+#define NS_PER_CYCLE (NS_PER_SEC / CYCLES_PER_SEC)
+#define NS_TO_CYCLES(n) ((n) / NS_PER_CYCLE)
+
+#define wait_ns(x)                                  \
+    do {                                            \
+        for (int i = 0; i < NS_TO_CYCLES(x); i++) { \
+            __asm__ volatile("nop\n\t"              \
+                             "nop\n\t"              \
+                             "nop\n\t"              \
+                             "nop\n\t"              \
+                             "nop\n\t"              \
+                             "nop\n\t");            \
+        }                                           \
+    } while (0)
+
 void init_rgblight_custom(void);
 void set_color_custom(int index, uint8_t r, uint8_t g, uint8_t b);
 void set_color_all_custom(uint8_t r, uint8_t g, uint8_t b);
 void flush_rgblight_custom(void);
+void send_ws2812_byte(uint8_t b);
 
 const rgblight_driver_t rgblight_driver = {
   .init          = init_rgblight_custom,
@@ -83,9 +111,6 @@ const rgblight_driver_t rgblight_driver = {
 
 /* Global array to hold backlight LED color info */
 ws2812_led_t backlight_leds[RGBLIGHT_LED_COUNT];
-
-void init_rgblight_custom(void) {
-}
 
 void set_color_custom(int index, uint8_t r, uint8_t g, uint8_t b) {
   backlight_leds[index].r = r;
@@ -99,6 +124,39 @@ void set_color_all_custom(uint8_t r, uint8_t g, uint8_t b) {
   }
 }
 
-void flush_rgblight_custom(void) {
+void init_rgblight_custom(void) {
+  palSetLineMode(WS2812_RGBLIGHT_DI_PIN, PAL_MODE_OUTPUT_PUSHPULL);
 }
 
+void flush_rgblight_custom(void) {
+  chSysLock(); // Disable interrupts
+  for (int i=0; i<RGBLIGHT_LED_COUNT; i++) {
+    send_ws2812_byte(backlight_leds[i].g);
+    send_ws2812_byte(backlight_leds[i].r);
+    send_ws2812_byte(backlight_leds[i].b);
+  }
+  wait_ns(WS2812_RES);
+  chSysUnlock(); // Re-enable interrupts
+}
+
+/*
+ * Bitbang out the color information on the data line.
+ */
+void send_ws2812_byte(uint8_t b) {
+  for (unsigned char bit=0; bit<8; bit++) {
+    bool is_one = b & (1 << (7-bit));
+    if (is_one) {
+      gpio_write_pin_high(WS2812_RGBLIGHT_DI_PIN);
+      wait_ns(WS2812_T1H);
+      gpio_write_pin_low(WS2812_RGBLIGHT_DI_PIN);
+      wait_ns(WS2812_T1L);
+    } else {
+      gpio_write_pin_high(WS2812_RGBLIGHT_DI_PIN);
+      wait_ns(WS2812_T0H);
+      gpio_write_pin_low(WS2812_RGBLIGHT_DI_PIN);
+      wait_ns(WS2812_T0L);
+    }
+  }
+}
+
+#endif
