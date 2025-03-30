003dcc7c6a4ec5a3100aa44d206d2c4c9e5e1afe
Sean Caulfield <sean@yak.net>
2025-03-29 21:05:38 -0400

    wip

--- keyboards/tab/swagkeys/transition_lite_1k/transition_lite_1k.c
+++ keyboards/tab/swagkeys/transition_lite_1k/transition_lite_1k.c
@@ -16,18 +16,6 @@
 #include "quantum.h"
 #include "ws2812.h"
 
-void init_rgblight_custom(void);
-void set_color_custom(int index, uint8_t r, uint8_t g, uint8_t b);
-void set_color_all_custom(uint8_t r, uint8_t g, uint8_t b);
-void flush_rgblight_custom(void);
-
-const rgblight_driver_t rgblight_driver = {
-  .init          = init_rgblight_custom,
-  .set_color     = set_color_custom,
-  .set_color_all = set_color_all_custom,
-  .flush         = flush_rgblight_custom,
-};
-
 /**************************** Matrix *********************************/
 // 矩阵初始化之前执行
 void keyboard_pre_init_kb(void) {
@@ -69,13 +57,46 @@ bool rgb_matrix_indicators_kb(void) {
 }
 #endif
 
+/*
+ * Custom RGBLight driver is required, as the WS2812 LEDs for it are on a
+ * different pin from the RGB matrix's WS2812 LEDs. Since the ws2812 driver
+ * assumes a single, macro-defined pin for WS2812 usage, meaning we can't use
+ * it for both RGB backlighting and RGB matrix.
+ *
+ * This mostly just steals the bitbang WS2812 code, as we don't have many LEDs
+ * for the backlight and I want at least get something working, even if it's
+ * not the most optimal.
+ *
+ */
+
+void init_rgblight_custom(void);
+void set_color_custom(int index, uint8_t r, uint8_t g, uint8_t b);
+void set_color_all_custom(uint8_t r, uint8_t g, uint8_t b);
+void flush_rgblight_custom(void);
+
+const rgblight_driver_t rgblight_driver = {
+  .init          = init_rgblight_custom,
+  .set_color     = set_color_custom,
+  .set_color_all = set_color_all_custom,
+  .flush         = flush_rgblight_custom,
+};
+
+/* Global array to hold backlight LED color info */
+ws2812_led_t backlight_leds[RGBLIGHT_LED_COUNT];
+
 void init_rgblight_custom(void) {
 }
 
 void set_color_custom(int index, uint8_t r, uint8_t g, uint8_t b) {
+  backlight_leds[index].r = r;
+  backlight_leds[index].g = g;
+  backlight_leds[index].b = b;
 }
 
 void set_color_all_custom(uint8_t r, uint8_t g, uint8_t b) {
+  for (int i=0; i < RGBLIGHT_LED_COUNT; i++) {
+    set_color_custom(i, r, g, b);
+  }
 }
 
 void flush_rgblight_custom(void) {
