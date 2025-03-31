4d33e5b90d3efa31a343e4527ac60363d654e7aa
Sean Caulfield <sean@yak.net>
2025-03-31 01:19:21 -0400

    standardize

--- keyboards/vertex/cycle8/keymaps/seancaulfield/keymap.c
+++ keyboards/vertex/cycle8/keymaps/seancaulfield/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,    KC_DEL,  KC_END,  KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,            KC_UP,
-    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LALT, KC_LGUI, FNLINUX, KC_LCTL,   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_LALT, FNLINUX, KC_LGUI, KC_LCTL,   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_BASE_MACOS] = LAYOUT_tkl_iso(
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_ENT,    KC_DEL,  KC_END,  KC_PGDN,
     KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,            KC_UP,
-    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_LCMD, KC_LOPT, FNMACOS, KC_LCTL,   KC_LEFT, KC_DOWN, KC_RGHT
+    KC_LCTL, KC_LOPT, KC_LCMD,                            KC_SPC,                             KC_LCMD, FNMACOS, KC_LOPT, KC_LCTL,   KC_LEFT, KC_DOWN, KC_RGHT
   ),
 
   [_BASE_LINUX] = LAYOUT_tkl_iso(
@@ -51,8 +51,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
     RGB_TOG, RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______,   _______, _______, _______,
     _______, RGB_RMD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,            _______,
-    _______, _______, _______,                            _______,                            _______, _______, _______, _______,   _______, _______, _______
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,            KC_VOLU,
+    _______, _______, _______,                            _______,                            _______, _______, _______, _______,   KC_MPRV, KC_VOLD, KC_MNXT
   ),
 
 };
