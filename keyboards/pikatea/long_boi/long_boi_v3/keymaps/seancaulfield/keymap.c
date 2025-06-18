4fbc5f1398901eb31d44175191b0a2bf0a836d68
Sean Caulfield <sean@yak.net>
2025-06-18 15:06:51 -0400

    forgot tab, placeholder key for KC_NUHS

--- keyboards/pikatea/long_boi/long_boi_v3/keymaps/seancaulfield/keymap.c
+++ keyboards/pikatea/long_boi/long_boi_v3/keymaps/seancaulfield/keymap.c
@@ -29,14 +29,14 @@ enum {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE_LINUX] = LAYOUT(
-         KC_F13,            KC_GRV,  KC_MINS, KC_EQL,  KC_ESC,   KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  KC_INS,  KC_HOME, KC_PGUP,  KC_P7, KC_P8,   KC_P9,   KC_PAST,
-                            KC_1,    KC_2,    KC_3,    KC_4,     KC_CAPS, KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_DEL,  KC_END,  KC_PGDN,  KC_P4, KC_P5,   KC_P6,   KC_PMNS,
+         KC_F13,            KC_GRV,  KC_MINS, KC_EQL,  KC_ESC,   KC_TAB,  KC_Q,    KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_INS,  KC_HOME, KC_PGUP,  KC_P7, KC_P8,   KC_P9,   KC_PAST,
+                            KC_1,    KC_2,    KC_3,    KC_4,     KC_CAPS, KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_F4,   KC_ENT,  KC_DEL,  KC_END,  KC_PGDN,  KC_P4, KC_P5,   KC_P6,   KC_PMNS,
 KC_PSCR, KC_SCRL, KC_PAUS,  KC_5,    KC_6,    KC_7,    KC_8,     KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,             KC_P1, KC_P2,   KC_P3,   KC_PPLS,
 KC_F1,   KC_F2,   KC_F3,    KC_BSPC, KC_F14,  KC_9,    KC_0,     KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                KC_RALT, FNLINUX, KC_RGUI, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0, KC_PDOT, KC_PSLS, KC_PENT
   ),
   [_BASE_MACOS] = LAYOUT(
-         KC_F13,            KC_GRV,  KC_MINS, KC_EQL,  KC_ESC,   KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV,  KC_INS,  KC_HOME, KC_PGUP,  KC_P7, KC_P8,   KC_P9,   KC_PAST,
-                            KC_1,    KC_2,    KC_3,    KC_4,     KC_CAPS, KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,  KC_DEL,  KC_END,  KC_PGDN,  KC_P4, KC_P5,   KC_P6,   KC_PMNS,
+         KC_F13,            KC_GRV,  KC_MINS, KC_EQL,  KC_ESC,   KC_TAB,  KC_Q,    KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_INS,  KC_HOME, KC_PGUP,  KC_P7, KC_P8,   KC_P9,   KC_PAST,
+                            KC_1,    KC_2,    KC_3,    KC_4,     KC_CAPS, KC_A,    KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT, KC_F4,   KC_ENT,  KC_DEL,  KC_END,  KC_PGDN,  KC_P4, KC_P5,   KC_P6,   KC_PMNS,
 KC_PSCR, KC_SCRL, KC_PAUS,  KC_5,    KC_6,    KC_7,    KC_8,     KC_LSFT, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                   KC_UP,             KC_P1, KC_P2,   KC_P3,   KC_PPLS,
 KC_F1,   KC_F2,   KC_F3,    KC_BSPC, KC_F14,  KC_9,    KC_0,     KC_LCTL, KC_LOPT, KC_LCMD,                   KC_SPC,                KC_RCMD, FNMACOS, KC_ROPT, KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0, KC_PDOT, KC_PSLS, KC_PENT
   ),
