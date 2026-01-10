e2459d2ab86041437f4b7e50dd20d2e8a8ec8c98
Sean Caulfield <sean@yak.net>
2026-01-10 13:12:19 -0500

    update to proper RGB matrix keycodes part the first

--- keyboards/mechlovin/infinity875/keymaps/seancaulfield_wkl/keymap.c
+++ keyboards/mechlovin/infinity875/keymaps/seancaulfield_wkl/keymap.c
@@ -27,7 +27,6 @@ enum my_layers {
 
 #define FNLINUX MO(_FN_LINUX)
 #define FNMACOS MO(_FN_MACOS)
-#define RGB_RMD RGB_RMOD
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE_LINUX] = LAYOUT_all(
@@ -35,30 +34,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,    KC_INS,  KC_HOME, KC_PGUP,
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RGUI,                     KC_UP,
-        KC_LCTL,   XXXXXXX, KC_LALT,                            KC_SPC,                             XXXXXXX, FNLINUX, XXXXXXX, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, FNLINUX,                     KC_UP,
+        KC_LCTL,   XXXXXXX, KC_LALT,                            KC_SPC,                             XXXXXXX, KC_RALT, XXXXXXX, KC_RGUI,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_BASE_MACOS] = LAYOUT_all(
         KC_ESC,    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,             KC_PSCR, KC_SCRL, KC_PAUS,
         KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,    KC_INS,  KC_HOME, KC_PGUP,
         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,            KC_DEL,  KC_END,  KC_PGDN,
         KC_CAPS,   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_ROPT,                     KC_UP,
-        KC_LCTL,   XXXXXXX, KC_LCMD,                            KC_SPC,                             XXXXXXX, FNMACOS, XXXXXXX, KC_RCTL,            KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,   KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, FNMACOS,                     KC_UP,
+        KC_LCTL,   XXXXXXX, KC_LCMD,                            KC_SPC,                             XXXXXXX, KC_RCMD, XXXXXXX, KC_ROPT,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
     [_FN_LINUX] = LAYOUT_all(
         QK_BOOT,   XXXXXXX, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY, KC_MNXT, KC_BRID, KC_BRIU, _______, _______, _______, KC_F13,              _______, _______, EE_CLR,
         _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
-        RGB_TOG,   RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
-        _______,   RGB_RMD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,          _______,
+        RM_TOGG,   RM_NEXT, RM_VALU, RM_HUEU, RM_SATU, RM_SPDU, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
+        _______,   RM_PREV, RM_VALD, RM_HUED, RM_SATD, RM_SPDD, _______, _______, _______, _______, _______, _______,          _______,
         _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                      KC_VOLU,
         _______,   XXXXXXX, _______,                            _______,                            XXXXXXX, _______, XXXXXXX, _______,             KC_MPRV, KC_VOLD, KC_MNXT
     ),
     [_FN_MACOS] = LAYOUT_all(
         QK_BOOT,   XXXXXXX, KC_BRID, KC_BRIU, KC_MCTL, KC_FIND, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,             _______, _______, EE_CLR,
         _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______,
-        RGB_TOG,   RGB_MOD, RGB_VAI, RGB_HUI, RGB_SAI, RGB_SPI, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
-        _______,   RGB_RMD, RGB_VAD, RGB_HUD, RGB_SAD, RGB_SPD, _______, _______, _______, _______, _______, _______,          _______,
+        RM_TOGG,   RM_NEXT, RM_VALU, RM_HUEU, RM_SATU, RM_SPDU, _______, _______, _______, _______, _______, _______, _______, _______,             _______, _______, _______,
+        _______,   RM_PREV, RM_VALD, RM_HUED, RM_SATD, RM_SPDD, _______, _______, _______, _______, _______, _______,          _______,
         _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                      KC_VOLU,
         _______,   XXXXXXX, _______,                            _______,                            XXXXXXX, _______, XXXXXXX, _______,             KC_MPRV, KC_VOLD, KC_MNXT
     )
